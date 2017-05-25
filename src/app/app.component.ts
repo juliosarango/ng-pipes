c5b4fe3f6f3e0898fd208b0c04b57834de311815
Julio Sarango <jsarangoq@gmail.com>
2017-05-24 23:28:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  nombre:string = "Julio Sarango";
+
+  arreglo:number[] = [1,2,3,4,5,6,7,8,8,9,10];
+
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+  salario:number = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "La mas grande",
+    },
+    poder: "regeneracion",
+    armas: "cuchillos en las manos",
+
+  };
+
 }
